2920e782fb877123166f6de0a28c573ca44203b4
Carissa Garde <47163038+beesbuzzingby@users.noreply.github.com>
2020-04-03 14:03:00 -0400

    Added code to build executable game file
    When you run python setup.py build on pycharm it creates an executable application under the "build" folder
    Imported cx_Freeze library to do this

---
+++ project/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import cx_Freeze
+
+execute = [cx_Freeze.Executable("game.py")]
+
+cx_Freeze.setup(
+    name='Escape to Miami',
+    version='1.1',
+    options={'build_exe': {"packages": ['pygame'],
+                           'include_files': ['miami-sunset.jpg', 'alligator.png', 'BOYCOTT_.ttf', 'After_Shok.ttf']}},
+    packages=[''],
+    url='http://escapetomiami.com',
+    license='',
+    author='Carissa Garde',
+    author_email='carissag@yahoo.com',
+    description='text-based game',
+    executables=execute
+)
