b53097c13b20cb05faa541a6a9f00bf1825b2011
Yugoslothia <46723398+Yugoslothia@users.noreply.github.com>
2020-04-09 21:33:25 -0400

    Basic inventory system for user (w/ test)
    Includes a basic test at end of file to ensure functions are working (obviously can be removed or moved later on)

---
+++ project/inventory.py
@@ -0,0 +1,61 @@
+# Escape to Miami
+#
+class Inventory:
+    def __init__(self):
+        self._inv = {}
+        self._inv_amount = 0
+        self._max_amount = 5
+
+    # use 2 variables to get item
+    def getInventory(self):
+        return self._inv_amount, self._inv
+
+    def expandInventory(self, n):
+        self._max_amount += n
+
+    def addItem(self, item):
+        if self._inv_amount < self._max_amount:
+            if item in self._inv:
+                self._inv[item] += 1
+            else:
+                self._inv[item] = 1
+            self._inv_amount += 1
+            return True
+        else:
+            return False
+
+    def useItem(self, item):
+        if self._inv_amount == 0:
+            return False
+        else:
+            if item in self._inv:
+                self._inv[item] -= 1
+                self._inv_amount -= 1
+                if self._inv[item] == 0:
+                    del self._inv[item]
+                return True
+            else:
+                return False
+
+
+# short test to see if all/most functions of inventory work
+if __name__ == "__main__":
+    inventory = Inventory()
+    print(inventory.addItem("Gas Can"))
+    print(inventory.addItem("Snickers"))
+    print(inventory.addItem("Gas Can"))
+    print(inventory.addItem("A Whole ass Alligator"))
+    print(inventory.addItem(3))
+    if inventory.addItem("Chimken Nug") is False:
+        print(inventory.useItem("Snickers"))
+        print(inventory.addItem("Chimken Nug"))
+    print(inventory.getInventory())
+    inventory.expandInventory(1)
+    print(inventory.addItem("More Chimken Nug"))
+    print(inventory.getInventory())
+    print(inventory.addItem("Gun"))
+    print(inventory.useItem("God"))
+    print(inventory.useItem("Chimken Nug"))
+    print(inventory.useItem("Gas Can"))
+    print(inventory.getInventory())
+
